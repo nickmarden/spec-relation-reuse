eccc58ab3f523e702510a29fb6fccaf150a3aa91
Nick Marden <nick@marden.org>
2014-02-12 14:26:39 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpecRelationReuse::Application.config.secret_token = '6484d2fcdab4d592dafaf9520c0cea3ce7d3eda6059c02aae652292c76f7a8eee326f740dca17e94cd8fededd5ca59fde21ad2766a80e70a004a08a52c021636'
